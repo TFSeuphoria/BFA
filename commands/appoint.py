cf895b11f4d096ed815d5db0ea52b9916bf5e07e
TFSeuphoria <jacksonl5941@gmail.com>
2026-05-25 23:06:07 -0400

    Updated appoint.py

--- commands/appoint.py
+++ commands/appoint.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import json
 import os
+import re
 
 from rolesData import (
     COMMISSIONER_ROLE,
@@ -10,6 +11,37 @@ from rolesData import (
 )
 
 from channelsData import DECISIONS_CHANNEL
+
+
+def load_json(path):
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            json.dump({}, f, indent=4)
+
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def get_team_emoji(team_role_id):
+    teams_json = load_json("teams.json")
+
+    for team_name, data in teams_json.items():
+        if data.get("role_id") == team_role_id:
+            return data.get("emoji", "")
+
+    return ""
+
+
+def set_embed_emoji_thumbnail(embed, emoji):
+    emoji_match = re.search(r"<a?:.+:(\d+)>", emoji)
+
+    if emoji_match:
+        emoji_id = emoji_match.group(1)
+        extension = "gif" if emoji.startswith("<a:") else "png"
+
+        embed.set_thumbnail(
+            url=f"https://cdn.discordapp.com/emojis/{emoji_id}.{extension}"
+        )
 
 
 class Appoint(commands.Cog):
@@ -34,24 +66,18 @@ class Appoint(commands.Cog):
             )
             return
 
-        if not os.path.exists("teams.json"):
-            await interaction.response.send_message(
-                "teams.json not found.",
-                ephemeral=True
-            )
-            return
+        teams_json = load_json("teams.json")
 
-        with open("teams.json", "r") as f:
-            teams = json.load(f)
-
-        if role.name not in teams:
+        if role.name not in teams_json:
             await interaction.response.send_message(
                 "That role is not a registered team.",
                 ephemeral=True
             )
             return
 
-        franchise_owner_role = interaction.guild.get_role(FRANCHISE_OWNER_ROLE)
+        franchise_owner_role = interaction.guild.get_role(
+            FRANCHISE_OWNER_ROLE
+        )
 
         await user.add_roles(role)
 
@@ -65,24 +91,44 @@ class Appoint(commands.Cog):
         except:
             pass
 
-        await interaction.response.send_message(
-            f"Appointed {user.mention} to {role.mention}",
-            ephemeral=True
-        )
+        team_emoji = get_team_emoji(role.id)
 
         embed = discord.Embed(
             title="New Appointment",
             color=discord.Color.green()
         )
 
-        embed.add_field(name="User", value=user.mention, inline=False)
-        embed.add_field(name="Team", value=role.mention, inline=False)
-        embed.add_field(name="Appointed By", value=interaction.user.mention, inline=False)
+        set_embed_emoji_thumbnail(embed, team_emoji)
 
-        decisions_channel = self.bot.get_channel(DECISIONS_CHANNEL)
+        embed.add_field(
+            name="User",
+            value=user.mention,
+            inline=False
+        )
+
+        embed.add_field(
+            name="Team",
+            value=f"{team_emoji} {role.mention}",
+            inline=False
+        )
+
+        embed.add_field(
+            name="Appointed By",
+            value=interaction.user.mention,
+            inline=False
+        )
+
+        decisions_channel = self.bot.get_channel(
+            DECISIONS_CHANNEL
+        )
 
         if decisions_channel:
             await decisions_channel.send(embed=embed)
+
+        await interaction.response.send_message(
+            f"Appointed {user.mention} to {role.mention}",
+            ephemeral=True
+        )
 
 
 async def setup(bot):
